02cf3010a23bac46793a8492805f4a15ee540c71
Noah Clark <Clarknoah@live.com>
2026-03-18 14:53:50 -0500

    initial commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./SideBar.module.css";
+
+function SideBar() {
+  return (
+    <div className={styles.SideBar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      <footer className={styles.footer}></footer>
+      <p className={styles.copyright}>
+        &copy; Copyright {new Date().gerFullYear()} by WorldWise Inc.
+      </p>
+    </div>
+  );
+}
+
+export default SideBar;
